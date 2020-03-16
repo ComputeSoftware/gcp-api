ebab746286df4746daaa6380e462ab7ff921ee63
Kenny Williams <kennyjwilli@gmail.com>
2020-03-16 12:46:19 -0700

    Ad logging to update-descriptors.sh Only commit & push if there are changes

--- bin/update-descriptors.sh
+++ bin/update-descriptors.sh
@@ -8,11 +8,16 @@ rm -rf $dir
 
 git clone git@github.com:ComputeSoftware/gcp-api-descriptors.git $dir
 
+echo "Updating descriptor files..."
 clojure -A:dev -m update-api-descriptors $dir
 
 cd $dir
 
-git add -A .
-
-git commit -a -m "Update descriptor files"
-git push
+if [[ $(git status --porcelain) ]]; then
+  echo "Changes dectected. Updating descriptors..."
+  git add -A .
+  git commit -a -m "Update descriptor files"
+  git push
+else
+  echo "No changes to push."
+fi

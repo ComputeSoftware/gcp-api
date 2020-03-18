af7273ea68c655e745584f2c4863dcc493416fe2
Kenny Williams <kennyjwilli@gmail.com>
2020-03-18 16:42:46 -0700

    Add support for updating descriptors in a branch

--- bin/update-descriptors.sh
+++ bin/update-descriptors.sh
@@ -3,16 +3,24 @@
 set -euo pipefail
 
 dir="gcp-api-descriptors"
+branch=$(git rev-parse --abbrev-ref HEAD)
 
 rm -rf $dir
 
+echo "Cloning gcp-api-descriptors branch:${branch}"
 git clone git@github.com:ComputeSoftware/gcp-api-descriptors.git $dir
 
+echo "Updating descriptor files..."
 clojure -A:dev -m update-api-descriptors $dir
 
 cd $dir
+git checkout -b ${branch}
 
-git add -A .
-
-git commit -a -m "Update descriptor files"
-git push
+if [[ $(git status --porcelain) ]]; then
+  echo "Changes dectected. Updating descriptors..."
+  git add -A .
+  git commit -a -m "Update descriptor files"
+  git push -u origin ${branch}
+else
+  echo "No changes to push."
+fi

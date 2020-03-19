d00e616a40a394d9ff53326b97af88f591c82894
Kenny Williams <kennyjwilli@gmail.com>
2020-03-18 17:16:27 -0700

    Force push

--- bin/update-descriptors.sh
+++ bin/update-descriptors.sh
@@ -9,18 +9,20 @@ rm -rf $dir
 
 echo "Cloning gcp-api-descriptors branch:${branch}"
 git clone git@github.com:ComputeSoftware/gcp-api-descriptors.git $dir
+cd $dir
+git checkout -b ${branch}
+cd ..
 
 echo "Updating descriptor files..."
 clojure -A:dev -m update-api-descriptors $dir
 
 cd $dir
-git checkout -b ${branch}
 
 if [[ $(git status --porcelain) ]]; then
   echo "Changes dectected. Updating descriptors..."
   git add -A .
   git commit -a -m "Update descriptor files"
-  git push -u origin ${branch}
+  git push -f -u origin ${branch}
 else
   echo "No changes to push."
 fi

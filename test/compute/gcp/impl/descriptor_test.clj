e9bf147e3eea7dcc98e9ea53e62828a5fb132090
Kenny Williams <kennyjwilli@gmail.com>
2020-04-17 11:52:31 -0700

    Fix test

--- test/compute/gcp/impl/descriptor_test.clj
+++ test/compute/gcp/impl/descriptor_test.clj
@@ -2,14 +2,8 @@
   (:require
     [clojure.test :refer :all]
     [provisdom.test.core :as t :refer [spec-check]]
-    [compute.gcp.impl.descriptor :as d]))
+    [compute.gcp.descriptor :as descriptor]))
 
 (deftest api-descriptor-resource-path-test
   (is (= "computesoftware/api-descriptors/compute/v1/api-descriptor.edn"
-         (d/api-descriptor-resource-path :compute "v1"))))
-
-(deftest openapi-op-lookup-gen-test
-  (is (spec-check `d/openapi-op-lookup
-                  {:recursion-limit  0
-                   :num-tests        100
-                   :coll-error-limit 1})))
+         (descriptor/api-descriptor-resource-path :compute "v1"))))

d318fe8cef7a7fa44e1c36d48e59b53ff138fec4
Kenny Williams <kennyjwilli@gmail.com>
2020-04-29 09:05:13 -0700

    Update dev helpers

--- env/playground.clj
+++ env/playground.clj
@@ -16,11 +16,20 @@
 (comment
   (def client (gcp-api/client {:api     :cloudbilling
                                :version "v1"}))
-  (gcp-api/invoke
-    client
-    {:op      "cloudbilling.services.skus.list"
-     :request {:parent "services/9662-B51E-5089"}
-     :as      :input-stream})
+  (def r
+    (gcp-api/invoke
+      client
+      {:op      "cloudbilling.services.skus.list"
+       :request {:pageSize 1000
+                 :parent   "services/9662-B51E-5089"}}))
+  (meta r)
+  (count (:skus r))
+  (:nextPageToken r)
+
+  (request/op-request-map
+    client {:op      "cloudbilling.services.skus.list"
+            :request {:parent    "services/6F81-5844-456A"
+                      :pageToken "AAyJ9cmzvJHcdfICci-scxkjojm1FKEPxog5LoYI0noRvc1b8GfvyKRbVlTc1NVqftUMtAmsK1hnkBJqGk1mt40ddyE6vBr8U3o5ObVYB45ryrLCriD4XRI="}})
 
   (request/op-request-map
     client
